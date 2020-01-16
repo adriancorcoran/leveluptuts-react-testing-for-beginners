465544edbc236a4de99d55720c2011b4db5b2b1f
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-01-16 22:05:36 +0000

    Mocking and testing function calls

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,11 @@
-import { add, total } from "./App";
+import { total } from "./App";
+// import { add, total } from "./App";
+
+// create mock function
+// - useful for:
+//    checking a function is called
+//    calling a function when not available e.g. import or db call
+const add = jest.fn(() => 3); //  auto return 3
 
 // Unit Test
 // only tests one thing
@@ -6,12 +13,18 @@ test("add", () => {
   // const value = add(1, 2);
   // expect(value).toBe(3);
   expect(add(1, 2)).toBe(3);
-  expect(add(5, 2)).toBe(7);
-  expect(add(-5, 2)).toBe(-3);
+  // expect(add(5, 2)).toBe(7);
+  // expect(add(-5, 2)).toBe(-3);
+
+  //  expect this function to be called this many times
+  expect(add).toHaveBeenCalledTimes(1);
+
+  //  expect this function to be called with these params
+  expect(add).toHaveBeenCalledWith(1, 2);
 });
 
 // Integration Test
 // code working together between components
-test("total", () => {
-  expect(total(5, 20)).toBe(`$25`);
-});
+// test("total", () => {
+//   expect(total(5, 20)).toBe(`$25`);
+// });
