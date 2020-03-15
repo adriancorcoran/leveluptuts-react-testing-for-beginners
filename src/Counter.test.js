84111acfb5f097f01b72ce36ddbd8acd4ac27f29
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-03-15 14:37:57 +0000

    #10 Events In React Testing Library

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -3,6 +3,9 @@ import React from "react";
 import { render, cleanup, fireEvent } from "react-testing-library";
 import Counter from "./Counter";
 
+// runs after each test and resets the data for the next test
+afterEach = cleanup;
+
 // test the Counter component
 test(<Counter />, () => {
   // const wrapper = render(<Counter />);
@@ -24,10 +27,15 @@ test(<Counter />, () => {
   expect(counterButton.tagName).toBe("BUTTON");
   // assert it starts at 0
   expect(counterButton.textContent).toBe("0");
+
   // click the button
   fireEvent.click(counterButton);
   expect(counterButton.textContent).toBe("1");
+
   // click the button
   fireEvent.click(counterButton);
   expect(counterButton.textContent).toBe("2");
+
+  // show the tree so far
+  debug();
 });
