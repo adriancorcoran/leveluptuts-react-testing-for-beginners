8b802f2d25b63140a9030813fd3f98397888cdc3
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-03-15 16:07:38 +0000

    #16 Negative Assertions & Testing With React Router

--- src/Movie.test.js
+++ src/Movie.test.js
@@ -1,15 +1,36 @@
 import React from "react";
 // clean up cleans up the dom tree after testing
 import { render, cleanup } from "react-testing-library";
+import { MemoryRouter } from "react-router-dom";
 import Movie from "./Movie";
 
 // runs after each test and resets the data for the next test
-afterEach = cleanup;
+afterEach(() => {
+  cleanup();
+  // the mock console was called in the first test, so we need to reset it for the next test
+  console.error.mockClear();
+});
 
 console.error = jest.fn();
 
 test("<Movie />", () => {
   render(<Movie />);
-  expect(console.error).toBeCalled();
-  // debug();
+  expect(console.error).toHaveBeenCalled();
+});
+
+const movie = {
+  title: "My Movie",
+  poster_path: "My_Poster.jpg",
+  id: "23"
+};
+
+test("<Movie /> with movie", () => {
+  render(
+    // this fakes the react router so we don't get the
+    // "Invariant Violation: You should not use <Link> outside a <Router>" error
+    <MemoryRouter>
+      <Movie movie={movie} />
+    </MemoryRouter>
+  );
+  expect(console.error).not.toHaveBeenCalled();
 });
