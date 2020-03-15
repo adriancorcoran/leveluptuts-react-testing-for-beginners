37c3f109024f7dc964c34216af5afac3db135864
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-03-15 15:27:37 +0000

    #14 Form Events With Controlled Inputs

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -10,7 +10,14 @@ class MovieForm extends Component {
     const { text } = this.state;
     return (
       <form data-testid="movie-form" onSubmit={() => submitForm({ text })}>
-        <input type="text" />
+        <label htmlFor="text">
+          Text
+          <input
+            type="text"
+            id="text"
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+        </label>
         <button>Submit</button>
       </form>
     );
--- src/MovieForm.test.js
+++ src/MovieForm.test.js
@@ -14,16 +14,30 @@ test("<MovieForm />", () => {
   // getByTestId will break if the elemnt is not found
   // queryByTestId may or may not find the element but won't break the test
   // you can use the assertion to specify whether finding the element is a good or bad thing
-  const { debug, queryByTestId, getByText } = render(
+  const { debug, queryByTestId, getByText, getByLabelText } = render(
     <MovieForm submitForm={onSubmit} />
   );
   expect(queryByTestId("movie-form")).toBeTruthy();
+
+  // get element by it's label text
+  // console.log(getByLabelText("Text").outerHTML);
+
+  // NEW SYNTAX
+  // get element by it's label text and set value
+  // getByLabelText("Text").value = "Hello";
+  // fireEvent.change(getByLabelText("Text"));
+
+  // MORE CONCISE
+  fireEvent.change(getByLabelText("Text"), { target: { value: "Hello" } });
 
   // check form is submitting properly
   fireEvent.click(getByText("Submit"));
 
   // check it was clicked
   expect(onSubmit).toHaveBeenCalledTimes(1);
+
+  // check the data of the form
+  expect(onSubmit).toHaveBeenCalledWith({ text: "Hello" });
 
   // debug();
 });
